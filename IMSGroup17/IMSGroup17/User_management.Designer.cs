ccdf01275b6867f7ac6ebe93eafc439ff4b6ac91
Hyperscope4000 <78375483+Hyperscope4000@users.noreply.github.com>
2021-03-04 09:44:52 +0000

    text boxes
    added text boxes to enter user info

---
+++ IMSGroup17/IMSGroup17/User_management.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace IMSGroup17
+{
+    partial class User_management
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEmpname = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtEmpname
+            // 
+            this.txtEmpname.Location = new System.Drawing.Point(12, 22);
+            this.txtEmpname.Name = "txtEmpname";
+            this.txtEmpname.Size = new System.Drawing.Size(179, 26);
+            this.txtEmpname.TabIndex = 0;
+            this.txtEmpname.Text = "Employee Name";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(12, 54);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(179, 26);
+            this.txtUser.TabIndex = 1;
+            this.txtUser.Text = "Username";
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(12, 86);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(179, 26);
+            this.txtPass.TabIndex = 2;
+            this.txtPass.Text = "Password";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(12, 118);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(179, 26);
+            this.txtPhone.TabIndex = 3;
+            this.txtPhone.Text = "Telephone number";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(12, 150);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(179, 26);
+            this.txtEmail.TabIndex = 4;
+            this.txtEmail.Text = "Email address";
+            // 
+            // User_management
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(947, 502);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.txtEmpname);
+            this.Name = "User_management";
+            this.Text = "User management";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEmpname;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtEmail;
+    }
+}
