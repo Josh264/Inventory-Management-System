498e89b4ad90010374d0a931c8aeae4895f7407d
Hyperscope4000 <78375483+Hyperscope4000@users.noreply.github.com>
2021-03-04 10:15:16 +0000

    Buttons
    Added buttons to user management

--- IMSGroup17/IMSGroup17/User_management.Designer.cs
+++ IMSGroup17/IMSGroup17/User_management.Designer.cs
@@ -34,11 +34,16 @@ namespace IMSGroup17
             this.txtPass = new System.Windows.Forms.TextBox();
             this.txtPhone = new System.Windows.Forms.TextBox();
             this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnHome = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtEmpname
             // 
-            this.txtEmpname.Location = new System.Drawing.Point(12, 22);
+            this.txtEmpname.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmpname.Location = new System.Drawing.Point(32, 79);
             this.txtEmpname.Name = "txtEmpname";
             this.txtEmpname.Size = new System.Drawing.Size(179, 26);
             this.txtEmpname.TabIndex = 0;
@@ -46,7 +51,8 @@ namespace IMSGroup17
             // 
             // txtUser
             // 
-            this.txtUser.Location = new System.Drawing.Point(12, 54);
+            this.txtUser.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUser.Location = new System.Drawing.Point(32, 111);
             this.txtUser.Name = "txtUser";
             this.txtUser.Size = new System.Drawing.Size(179, 26);
             this.txtUser.TabIndex = 1;
@@ -54,7 +60,8 @@ namespace IMSGroup17
             // 
             // txtPass
             // 
-            this.txtPass.Location = new System.Drawing.Point(12, 86);
+            this.txtPass.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPass.Location = new System.Drawing.Point(32, 143);
             this.txtPass.Name = "txtPass";
             this.txtPass.Size = new System.Drawing.Size(179, 26);
             this.txtPass.TabIndex = 2;
@@ -62,7 +69,8 @@ namespace IMSGroup17
             // 
             // txtPhone
             // 
-            this.txtPhone.Location = new System.Drawing.Point(12, 118);
+            this.txtPhone.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPhone.Location = new System.Drawing.Point(32, 175);
             this.txtPhone.Name = "txtPhone";
             this.txtPhone.Size = new System.Drawing.Size(179, 26);
             this.txtPhone.TabIndex = 3;
@@ -70,17 +78,58 @@ namespace IMSGroup17
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(12, 150);
+            this.txtEmail.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(32, 207);
             this.txtEmail.Name = "txtEmail";
             this.txtEmail.Size = new System.Drawing.Size(179, 26);
             this.txtEmail.TabIndex = 4;
             this.txtEmail.Text = "Email address";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(32, 420);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(179, 31);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(263, 420);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(179, 31);
+            this.btnEdit.TabIndex = 6;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(506, 420);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(179, 31);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnHome
+            // 
+            this.btnHome.Location = new System.Drawing.Point(756, 420);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(179, 31);
+            this.btnHome.TabIndex = 8;
+            this.btnHome.Text = "Home";
+            this.btnHome.UseVisualStyleBackColor = true;
             // 
             // User_management
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(947, 502);
+            this.Controls.Add(this.btnHome);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.txtEmail);
             this.Controls.Add(this.txtPhone);
             this.Controls.Add(this.txtPass);
@@ -100,5 +149,9 @@ namespace IMSGroup17
         private System.Windows.Forms.TextBox txtPass;
         private System.Windows.Forms.TextBox txtPhone;
         private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnHome;
     }
 }
